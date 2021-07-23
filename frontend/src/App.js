3228fbb694726442b9c25458a432f135baaf842c
lgaeun <gaeungaeun4837@gmail.com>
2021-07-23 18:44:48 +0900

    style: 💄apply Grommet

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Grommet} from 'grommet';
+// import MainPage from './pages/MainPage';
+import CategoryPage from './pages/CategoryPage';
+
+function App() {
+  return (
+    <Grommet className="App">
+      <CategoryPage />
+    </Grommet>
+  );
+}
+
+export default App;
