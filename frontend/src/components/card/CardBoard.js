928b44053597225659ec41162a5931af9c456eaa
lgaeun <gaeungaeun4837@gmail.com>
2021-07-23 18:34:57 +0900

    feature: ✨create card, cardboard

---
+++ frontend/src/components/card/CardBoard.js
@@ -0,0 +1,30 @@
+import JobCard from "./JobCard";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+const CardBoard = () => {
+  return (
+    <Container>
+      <JobCard
+        name={"네이버"}
+        body={"백엔드 개발자모집"}
+        liked={true}
+      ></JobCard>
+      <JobCard
+        name={"카카오"}
+        body={"프론트엔드 개발자모집"}
+        likd={false}
+      ></JobCard>
+      <JobCard></JobCard>
+    </Container>
+  );
+};
+export default CardBoard;
